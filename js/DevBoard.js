611e29e1764859fa90bafba52212af1c001a62e2
Topan-Roy <roytopan734@gmail.com>
2025-03-02 02:42:09 +0600

    added js file edit and  added blog html and blog js

--- js/DevBoard.js
+++ js/DevBoard.js
@@ -1,7 +1,9 @@
 document.getElementById("change-color").addEventListener("click", function() {
     document.getElementById("body").style.backgroundColor = "#" + Math.floor(Math.random() * 16777215.).toString(16);
 })
-
+document.getElementById('blog-question').addEventListener('click',function(){
+      window.location.href = "./blog.html"
+})
 
 const timeDay = document.getElementById("time-day")
 const timeDayP = document.createElement("p");
@@ -11,20 +13,7 @@ timeDay.appendChild(timeDayP)
 const timeDate = document.getElementById("time-date");
 const timeDateP = document.createElement("p");
 timeDateP.innerText = new Date().toLocaleDateString("en-BD", {month: "short", day: "numeric", year: "numeric"}).replaceAll(",", " ");
-timeDate.appendChild(timeDateP)
-// blogQuestionBtn.addEventListener("click", function() {
-//     window.location.href = "./blog.html"
-// })
-
-// const allBtnDisebol = document.querySelectorAll("#btnComplect");
-// for (let i = 0; i < allBtnDisebol.length; i++) {
-//     const allBtnComplect = allBtnDisebol[i];
-//     allBtnComplect.addEventListener("click", function(event) {
-//         event.preventDefault();
-//         alert("Board updated successfully");
-//         allBtnComplect.disabled = true;
-//     });
-// }
+timeDate.appendChild(timeDateP);
 
 const allBtnDisebol = document.querySelectorAll("#btnComplect"); 
 for (let i = 0; i < allBtnDisebol.length; i++) {
@@ -45,42 +34,21 @@ for (let i = 0; i < allBtnDisebol.length; i++) {
 
 
         const value = event.target.parentElement.parentElement.querySelector("h1").innerText;
-         const p = document.createElement("p");
-         const dateSet= new Date().toLocaleTimeString();
-        
-         p.innerText = `${value} at ${dateSet}`;
-         
+        const div = document.createElement("div");
+        div.innerHTML = `
+            <div class="mt-1 mx-2  p-1 rounded-xl">
+                <h3>You have completed the task ${value} at ${new Date().toLocaleTimeString()}</h3>
+            </div>
+        `
          const container = document.getElementById("container");
          if (container) {
-             container.appendChild(p);
+             container.appendChild(div);
          } else {
              console.log("Container not found!");
          }
     })
 }
 
-
-
-// const allBtnDiseboll = document.querySelectorAll("#btnComplect");
-// for (let i = 0; i < allBtnDiseboll.length; i++) {
-//     const allBtnComplectl = allBtnDiseboll[i];
-//     allBtnComplectl.addEventListener("click", function(event) {
-       
-//    const value = event.target.parentElement.parentElement.querySelector("h1").innerText;
-//    console.log(value)
-//     const p = document.createElement("p");
-//     const dateSet= new Date().toLocaleTimeString();
-   
-//     p.innerText = `${value} at ${dateSet}`;
-    
-//     const container = document.getElementById("container");
-//     if (container) {
-//         container.appendChild(p);
-//     } else {
-//         console.log("Container not found!");
-//     }
-//     });
-// }
-document.getElementById("clear-btn").addEventListener("click", function() {
+    document.getElementById("clear-btn").addEventListener("click", function() {
     container.innerHTML = "";
 })
